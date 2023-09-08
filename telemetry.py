3f32bf59255db8b741ac21248948e6b37631025c
krsun <krsun@umich.edu>
2023-09-08 14:26:26 -0400

    more functions to get RPIstuff

--- telemetry.py
+++ telemetry.py
@@ -1,8 +1,23 @@
-import os
+import subprocess
 
 
-print(os.system('vgencmd measure_temp'))
-print(os.system('free -m'))
-print(os.system('df -h'))
+# GET CPU TEMPERATURE 
+command = 'vgencmd measure_temp'
 
+# GET MEMORY DATA 
+command = 'free -m'
+result = subprocess.check_output(command, shell=True, universal_newlines=True)
+lines = result.strip().split('\n')
+data = lines[1].split()
+Total_Mem = float(data[1])
+Free_Mem = float(data[3])
 
+# GET STORAGE DATA
+command = 'df -h'
+result = subprocess.check_output(command, shell=True, universal_newlines=True)
+lines = result.strip().split('\n')
+data = lines[1].split()
+Total_Storage = data[1]
+Free_Storage = data[3]
+
+# WRITE TO CSV 
