40514cf804cd69a8219e78283ff34ecb67f5c0a6
krsun <krsun@umich.edu>
2023-09-12 20:44:44 -0400

    added sleep time

--- telemetry.py
+++ telemetry.py
@@ -1,12 +1,44 @@
 import csv
+import time 
 import subprocess
 
 def main():
-    # GET CPU TEMPERATURE (in celsius)
+    starttime = time.time()
+
+    while(True):
+        
+        #GET CPU TEMPERATURE (in celsius)
+        cpu_temp = temperature()
+
+        #GET MEMORY DATA 
+        total_mem, free_mem = mem_data()
+
+        # GET STORAGE DATA
+        total_storage, free_storage = storage_data()
+
+        # WRITE TO CSV 
+        data_line = [cpu_temp, total_mem, free_mem, total_storage, free_storage]
+        write_line(data_line)
+
+        # data collection runs once every 10 seconds
+        # remove the time taken by code to execute 
+        time.sleep(10.0 - ((time.time() - starttime) % 10.0))
+
+
+def write_line(new_data):
+    csv_file = "telemetry.csv"
+    with open(csv_file, mode='a') as file:
+        writer = csv.writer(file)
+        writer.writerow(new_data)
+
+
+def temperature():
     command = "vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*'"   # only pull out the number from string
     temp_result = float(subprocess.check_output(command, shell=True, universal_newlines=True))
+    return temp_result
 
-    # GET MEMORY DATA 
+
+def mem_data():
     command = "free -m"
     result = subprocess.check_output(command, shell=True, universal_newlines=True)
     # parse out the necessary data
@@ -16,7 +48,10 @@ def main():
     #print(Total_Mem)
     #print(Free_Mem)
 
-    # GET STORAGE DATA
+    return total_mem, free_mem
+
+
+def storage_data():
     command = 'df -h'
     result = subprocess.check_output(command, shell=True, universal_newlines=True)
     data = strip_shell_result(result)
@@ -25,12 +60,7 @@ def main():
     #print(total_storage)
     #print(free_storage)
 
-    # WRITE TO CSV 
-    data_line = [temp_result, total_mem, free_mem, total_storage, free_storage]
-    csv_file = "telemetry.csv"
-    with open(csv_file, mode='a') as file:
-        writer = csv.writer(file)
-        writer.writerow(data_line)
+    return total_storage, free_storage
 
 
 # used for memory and storage data
