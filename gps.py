54697255c8c22f7734e81de4342238f74c5affa9
deochand <deochand@umich.edu>
2023-09-14 14:39:53 -0400

    GPS test 1

--- gps.py
+++ gps.py
@@ -1,4 +1,24 @@
+import serial
+
+from ublox_gps import UbloxGps
+
+port = serial.Serial('/dev/tty1', baudrate=38400, timeout=1)
+gps = UbloxGps(port)
+
+def run():
+
+    try:
+        print("Listening for UBX Messages")
+        try:
+            geo = gps.geo_coords()
+            print("Longitude: ", geo.lon) 
+            print("Latitude: ", geo.lat)
+        except (ValueError, IOError) as err:
+            print(err)
+
+    finally:
+        port.close()
 
 
-def gps_info():
-    # do smoething 
+if __name__ == '__main__':
+    run()
