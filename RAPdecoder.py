102dd535e2d059698455fdd98e7d9c99ab8e684f
deochand <deochand@umich.edu>
2023-10-19 16:04:23 -0400

    RAP fixes

--- RAPdecoder.py
+++ RAPdecoder.py
@@ -24,8 +24,10 @@ currentIndex = currentIndex+2
 # stored in wrong -> converted to good endian for byte size
 length = data[currentIndex:currentIndex+4] #f500 
 length_decimal = length[2:] + length[0:2] # converts to big endian
-length_decimal = int(length_decimal, 16) # converts hexa to decimal
+length_decimal = int(length_decimal, 16) # converts hex to decimal
 currentIndex = currentIndex+4
+
+print(length_decimal)
 
 # Fletcher 16 implementation for header checksum
 sum1 = 0
@@ -38,13 +40,16 @@ headerChecksum_F16_decimal = (sum2 << 8) | sum1
 headerChecksum = data[currentIndex:currentIndex+4]
 headerChecksum = headerChecksum[2:] + headerChecksum[0:2] # converts to big endian
 headerChecksum_decimal = int(headerChecksum,16)
+currentIndex = currentIndex+4 # now at end of header checksum / beginning of real data
 
 dataBytes = length_decimal - 17
 realData = data[currentIndex:currentIndex+dataBytes*2]
 
+print('current index: ' + str(dataBytes))
 currentIndex = currentIndex+dataBytes*2
 
 checksum = data[currentIndex:currentIndex+4]
+checksum = checksum[2:] + checksum[0:2] # converts to big endian
 checksum_decimal = int(checksum,16)
 currentIndex = currentIndex+4
 
