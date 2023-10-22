1b01272b486a0debca62b66b4f8942618a55872a
krsun <krsun@umich.edu>
2023-10-22 17:22:05 -0400

    encoding and decoding work

--- RAP_data_decoder.py
+++ RAP_data_decoder.py
@@ -2,80 +2,96 @@ from math import sqrt
 import pdb
 # documentation link: https://docs.google.com/spreadsheets/d/1UO--kbqhnvFxWSW4DkALG8KMoHwPn-7fcskuqaRLpkQ/edit#gid=0
 
-# attempt with bit masking
-def extract_bits(data, offset, data_length):
-    # each datapoint is 4 bits 
-    # data length of 1 = 4 bits 
-    bitmask = (1 << data_length * 4) - 1
-    return (data & (bitmask << offset)) >> offset
 
+
+def little_to_big_endian(little_endian_hex):
+    # Convert little-endian hex string to bytes
+    little_endian_bytes = bytes.fromhex(little_endian_hex)
+    
+    # Reverse the byte order to convert from little-endian to big-endian
+    big_endian_bytes = little_endian_bytes[::-1]
+    
+    # Convert bytes back to hexadecimal and remove the '0x' prefix
+    big_endian_hex = big_endian_bytes.hex()
+    
+    return big_endian_hex
+
+
+def decode_data(data):
+    # convert to big endian 
+    start = 0*2
+    dist = 4*2
+    RTC_Unix_Time = int(little_to_big_endian(data[start:start+dist]), 16)
+
+    start = 4*2
+    dist = 2*2
+    num_resets = int(little_to_big_endian(data[start:start+dist]), 16)
+
+    start = 18*2
+    dist = 2*2
+    total_mem = int(little_to_big_endian(data[start:start+dist]), 16)
+
+    start = 20*2
+    dist = 2*2
+    free_mem = int(little_to_big_endian(data[start:start+dist]), 16)
+
+    start = 44*2
+    dist = 2*2
+    FCPU_temp0 = int(little_to_big_endian(data[start:start+dist]), 16)
+
+    start = 60*2
+    dist = 2*2
+    battery_voltage = int(little_to_big_endian(data[start:start+dist]), 16)
+
+    start = 62*2
+    dist = 2*2
+    battery_current = int(little_to_big_endian(data[start:start+dist]), 16)
+
+    start = 64*2
+    dist = 2*2
+    battery_temp = int(little_to_big_endian(data[start:start+dist]), 16)
+
+    start = 150*2
+    dist = 2*2
+    pos_Y_mag_X = int(little_to_big_endian(data[start:start+dist]), 16)
+
+    start = 152*2
+    dist = 2*2
+    pos_Y_mag_Y = int(little_to_big_endian(data[start:start+dist]), 16)
+
+    start = 154*2
+    dist = 2*2
+    pos_Y_mag_Z = int(little_to_big_endian(data[start:start+dist]), 16)
+
+
+    RTC_Unix_Time_conv = RTC_Unix_Time
+    num_resets_conv = num_resets
+    total_mem_conv = total_mem * 4
+    free_mem_conv = free_mem * 4
+    # pdb.set_trace()
+    FCPU_temp0_conv = -1481.96+sqrt(2.1962*1000000+(1.8639-(FCPU_temp0/1000.0))/(3.88/1000000))
+    battery_voltage_conv = (battery_voltage/1000)*(2.2) + 0
+    battery_current_conv = (battery_current/1000)*(2.0) + -3
+    battery_temp_conv = (battery_temp/1000)*(-85.54319) + 159.6493
+    pos_Y_mag_X_conv = pos_Y_mag_X / 219.0
+    pos_Y_mag_Y_conv = pos_Y_mag_Y / 219.0
+    pos_Y_mag_Z_conv = pos_Y_mag_Z / 219.0
+    pos_Y_mag_magnitude = sqrt(pos_Y_mag_X_conv*pos_Y_mag_X_conv + pos_Y_mag_Y_conv*pos_Y_mag_Y_conv + pos_Y_mag_Z_conv*pos_Y_mag_Z_conv)
+
+    print("RTC_Unix_Time_conv: " + str(RTC_Unix_Time_conv))
+    print("num_resets_conv: " + str(num_resets_conv))
+    print("total_mem_conv: " + str(total_mem_conv))
+    print("free_mem_conv: " + str(free_mem_conv))
+    print("FCPU_temp0_conv: " + str(FCPU_temp0_conv))
+    print("battery_voltage_conv: " + str(battery_voltage_conv))
+    print("battery_current_conv: " + str(battery_current_conv))
+    print("battery_temp_conv: " + str(battery_temp_conv))
+    print("pos_Y_mag_X_conv: " + str(pos_Y_mag_X_conv))
+    print("pos_Y_mag_Y_conv: " + str(pos_Y_mag_Y_conv))
+    print("pos_Y_mag_Z_conv: " + str(pos_Y_mag_Z_conv))
+    print("pos_Y_mag_magnitude: " + str(pos_Y_mag_magnitude))
 
 
 
 data = '862b1c65cc0609000b000900010023001f2fee7bef040100000000976a0700395b19006f2e2500004a4a3e0081067106fa0c60002800fa0ccf0e0d00ba0efc05ba06c80e10005f0d20001500f60c910056005e06610d8600f40cb400310a55013c0f6f00600664064909dc001c0f410056065c06d50745002d0f110065066506ea076b00320f18005b065e06fdfcb606b106b00600c03100160074ff5b000100e9ff8600efff6600c000c1ffcc00950a8f0ad209a309dd09b409620b4c0b88007402320a5b00b8338b334c20001000000002005d03000004000000ff723d00beffffffff'
-
-'''attempt at bit masking'''
-data_num = int(data,16)
-RTC_Unix_Time = extract_bits(data_num, offset=0, data_length=4)
-# convert RTC_Unix_Time to big endian 
-RTC_Unix_Time = (RTC_Unix_Time & 0xFF) << 0xFF | (RTC_Unix_Time >> 0xFF)
-num_resets = extract_bits(data_num, offset=4, data_length=2)
-total_mem = extract_bits(data_num, offset=18, data_length=2)
-free_mem = extract_bits(data_num, offset=20, data_length=2)
-FCPU_temp0 = extract_bits(data_num, offset=44, data_length=2)
-battery_voltage = extract_bits(data_num, offset=60, data_length=2)
-battery_current = extract_bits(data_num, offset=62, data_length=2)
-battery_temp = extract_bits(data_num, offset=64, data_length=2)
-pos_Y_mag_X = extract_bits(data_num, offset=150, data_length=2)
-pos_Y_mag_Y = extract_bits(data_num, offset=152, data_length=2)
-pos_Y_mag_Z = extract_bits(data_num, offset=154, data_length=2)
-
-
-'''using strings method'''
-# convert to big endian 
-# RTC_Unix_Time = data[0:0+4]
-# RTC_Unix_Time = int(RTC_Unix_Time[2:] + RTC_Unix_Time[0:2], 16)
-
-# num_resets = int(data[4:4+2], 16)
-# total_mem = int(data[18:18+2], 16)
-# free_mem = int(data[20:20+2], 16)
-# FCPU_temp0 = int(data[44:44+2], 16)
-# battery_voltage = int(data[60:60+2], 16)
-# battery_current = int(data[62:62+2], 16)
-# battery_temp = int(data[64:64+2], 16)
-# pos_Y_mag_X = int(data[150:150+2], 16)
-# pos_Y_mag_Y = int(data[152:152+2], 16)
-# pos_Y_mag_Z = int(data[154:154+2], 16)
-
-
-
-RTC_Unix_Time_conv = RTC_Unix_Time
-num_resets_conv = num_resets
-total_mem_conv = total_mem * 4
-free_mem_conv = free_mem * 4
-# pdb.set_trace()
-FCPU_temp0_conv = -1481.96+sqrt(2.1962*1000000+(1.8639-(FCPU_temp0/1000.0))/(3.88/1000000))
-battery_voltage_conv = (battery_voltage/1000)*(2.2) + 0
-battery_current_conv = (battery_current/1000)*(2.0) + -3
-battery_temp_conv = (battery_temp/1000)*(-85.54319) + 159.6493
-pos_Y_mag_X_conv = pos_Y_mag_X / 219.0
-pos_Y_mag_Y_conv = pos_Y_mag_Y / 219.0
-pos_Y_mag_Z_conv = pos_Y_mag_Z / 219.0
-pos_Y_mag_magnitude = sqrt(pos_Y_mag_X_conv*pos_Y_mag_X_conv + pos_Y_mag_Y_conv*pos_Y_mag_Y_conv + pos_Y_mag_Z_conv*pos_Y_mag_Z_conv)
-
-print("RTC_Unix_Time_conv: " + str(RTC_Unix_Time_conv))
-print("num_resets_conv: " + str(num_resets_conv))
-print("total_mem_conv: " + str(total_mem_conv))
-print("free_mem_conv: " + str(free_mem_conv))
-print("FCPU_temp0_conv: " + str(FCPU_temp0_conv))
-print("battery_voltage_conv: " + str(battery_voltage_conv))
-print("battery_current_conv: " + str(battery_current_conv))
-print("battery_temp_conv: " + str(battery_temp_conv))
-print("pos_Y_mag_X_conv: " + str(pos_Y_mag_X_conv))
-print("pos_Y_mag_Y_conv: " + str(pos_Y_mag_Y_conv))
-print("pos_Y_mag_Z_conv: " + str(pos_Y_mag_Z_conv))
-print("pos_Y_mag_magnitude: " + str(pos_Y_mag_magnitude))
-
-
-
-
+decode_data(data)
---
+++ encoder.py
@@ -0,0 +1,134 @@
+import pdb
+
+def tester():
+    data = {
+        "RTC_unix_time": 1696344966, 
+        "Num_resets": 1740, 
+        "tot_Mem": 126904, 
+        "free_Mem": 5052,
+        "FCPU_temp0": 17.194056992788774,
+        "Battery_voltage": 8.294,
+        "Battery_current": 0.06400000000000006,
+        "Battery_temp": 12.343926820000007,
+        "pos_y_mag_x": 0.2237442922374429,
+        "pos_y_mag_y": 0.1004566210045662,
+        "pos_y_mag_z": 298.61187214611874
+        }
+
+    print(encode(data))
+
+# data comes in  a dictionary
+def encode(data):
+    packet_list = ['0'] * 245 * 2
+
+    sync = 'abcd'
+    place_bytes(packet_list, sync, 0, 2)
+    
+    pid = '0000'
+    place_bytes(packet_list, pid, 2, 2)
+
+    sid = '0042'
+    place_bytes(packet_list, sid, 4, 2)
+    
+    flags = '03'
+    place_bytes(packet_list, flags, 6, 1)
+
+    length = '00f5'
+    place_bytes(packet_list, length, 7, 2)
+    
+    # do checksum and other stuff
+    # Fletcher 16 implementation for header checksum
+    sum1 = 0
+    sum2 = 0
+    for i in range(0,18,2):
+        # pdb.set_trace()
+        sum1 = (sum1 + int(''.join(packet_list[i:i+2]),16)) % 255
+        sum2 = (sum2 + sum1) % 255
+    headerChecksum_F16_decimal = (sum2 << 8) | sum1
+    headerChecksum_F16_hex = hex(headerChecksum_F16_decimal)[2:]
+
+    place_bytes(packet_list, headerChecksum_F16_hex, 9, 2)
+
+    hmac = 'b0151c40'
+    place_bytes(packet_list, hmac, 241, 4)
+
+
+    # first try to get data
+    RTC_unix_time_convt = data.get("RTC_unix_time") or 0
+    Num_resets_convt = data.get("Num_resets") or 0
+    tot_Mem_convt = data.get("tot_Mem") or 0
+    free_Mem_convt = data.get("free_Mem") or 0
+    FCPU_temp0_convt = data.get("FCPU_temp0") or 0
+    Battery_voltage_convt = data.get("Battery_voltage") or 0
+    Battery_current_convt = data.get("Battery_current") or 0
+    Battery_temp_convt = data.get("Battery_temp") or 0
+    pos_y_mag_x_convt = data.get("pos_y_mag_x") or 0
+    pos_y_mag_y_convt= data.get("pos_y_mag_y") or 0
+    pos_y_mag_z_convt = data.get("pos_y_mag_z") or 0
+
+    # then convert everything to right format 
+    RTC_unix_time = RTC_unix_time_convt
+    Num_resets = Num_resets_convt
+    tot_Mem = int(tot_Mem_convt / 4)
+    free_Mem = int(free_Mem_convt / 4)
+    FCPU_temp0 = int(((((FCPU_temp0_convt+1481.96)**2 - 2.1962*1000000)*(3.88/1000000)) - 1.8639)*-1000.0)
+    Battery_voltage = int(Battery_voltage_convt *(1000/2.2))
+    Battery_current = int((Battery_current_convt + 3)*1000/2)
+    Battery_temp = int((Battery_temp_convt - 159.6493) * (1000 / -85.54319)) 
+    pos_y_mag_x = int(pos_y_mag_x_convt * 219.0)
+    pos_y_mag_y = int(pos_y_mag_y_convt * 219.0)
+    pos_y_mag_z = int(pos_y_mag_z_convt * 219.0)
+   
+    # put converted value into hex 
+    RTC_unix_time_hex = hex(RTC_unix_time)[2:]
+    Num_resets_hex = hex(Num_resets)[2:]
+    tot_Mem_hex = hex(tot_Mem)[2:]
+    free_Mem_hex = hex(free_Mem)[2:]
+    FCPU_temp0_hex = hex(FCPU_temp0)[2:]
+    Battery_voltage_hex = hex(Battery_voltage)[2:]
+    Battery_current_hex = hex(Battery_current)[2:]
+    Battery_temp_hex = hex(Battery_temp)[2:]
+    pos_y_mag_x_hex = hex(pos_y_mag_x)[2:]
+    pos_y_mag_y_hex = hex(pos_y_mag_y)[2:]
+    pos_y_mag_z_hex = hex(pos_y_mag_z)[2:]
+    
+    # put hex values into the right positions 
+    # data length and beacon_offset are in bytes 
+    pre_data_bytes = 11
+    place_bytes(packet_list, value=RTC_unix_time_hex, beacon_offset=pre_data_bytes+0, data_length=4)
+    place_bytes(packet_list, value=Num_resets_hex, beacon_offset=pre_data_bytes+4, data_length=2)
+    place_bytes(packet_list, value=tot_Mem_hex, beacon_offset=pre_data_bytes+18, data_length=2)
+    place_bytes(packet_list, value=free_Mem_hex, beacon_offset=pre_data_bytes+20, data_length=2)
+    place_bytes(packet_list, value=FCPU_temp0_hex, beacon_offset=pre_data_bytes+44, data_length=2)
+    place_bytes(packet_list, value=Battery_voltage_hex, beacon_offset=pre_data_bytes+60, data_length=2)
+    place_bytes(packet_list, value=Battery_current_hex, beacon_offset=pre_data_bytes+62, data_length=2)
+    place_bytes(packet_list, value=Battery_temp_hex, beacon_offset=pre_data_bytes+64, data_length=2)
+    place_bytes(packet_list, value=pos_y_mag_x_hex, beacon_offset=pre_data_bytes+150, data_length=2)
+    place_bytes(packet_list, value=pos_y_mag_y_hex, beacon_offset=pre_data_bytes+152, data_length=2)
+    place_bytes(packet_list, value=pos_y_mag_z_hex, beacon_offset=pre_data_bytes+154, data_length=2)
+
+
+    # Fletcher 16 implementation for data checksum
+    sum1 = 0
+    sum2 = 0
+    for i in range(22,478,2):
+        sum1 = (sum1 + int(''.join(packet_list[i:i+2]),16)) % 255
+        sum2 = (sum2 + sum1) % 255
+    dataChecksum_F16_decimal = (sum2 << 8) | sum1
+    dataChecksum_F16_hex = hex(dataChecksum_F16_decimal)[2:]    
+    place_bytes(packet_list, dataChecksum_F16_hex, 239, 2)
+
+    return ''.join(packet_list)
+
+
+# start placing characters into the packet list starting from the back 
+def place_bytes(packet_list, value, beacon_offset, data_length):
+    end_position = beacon_offset*2 + data_length * 2 - 1
+
+    for char in value[::-1]:
+        packet_list[end_position] = char
+        end_position -= 1
+
+
+if __name__ == "__main__":
+    tester()
