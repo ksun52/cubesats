e2c5ec483c34ade18d1736630d8643c9db2a5124
deochand <deochand>
2023-11-02 14:47:15 -0400

    camera changes

--- camera.py
+++ camera.py
@@ -7,24 +7,29 @@ Record a 30 second video and then take a thumbnail picture
 """
 def camera_run():
     # TODO set proper camera resolution and framerate 
-    camera = PiCamera(resolution =(1024, 768), framerate=10)
+    camera = picamera.PiCamera(resolution =(1024, 768), framerate=10)
     
     time.sleep(2)
-    
-    while True:
+    i= 3
+    while i>0:
         timestamp = time.time()
         filenum = 1
         
         thumbnailfile = f"thumbnails/{filenum}_{str(int(timestamp))}.jpg"
         camera.capture(thumbnailfile)
 
+	print("thumbnail captured")
+
         videofile = f"videos/{filenum}_{str(int(timestamp))}.h264"
         camera.start_recording(videofile)
+	print("video starting")
         camera.wait_recording(10)
         camera.stop_recording()
+	print("video ending")
 
         filenum += 1
-        
+	
+	i -= 1        
 
 
 
