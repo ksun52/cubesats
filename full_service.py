c8ed370a9c94c663a66786bdc98e935da39546af
krsun <krsun@umich.edu>
2023-11-09 15:04:49 -0500

    added full service function to run everything at once

---
+++ full_service.py
@@ -0,0 +1,21 @@
+import threading 
+import telemetry
+import mag_run
+import imu_run
+import time
+
+def main():
+    magnetometer_thread = threading.Thread(target=telemetry.main)
+    imu_thread = threading.Thread(target=telemetry.main)
+    telemetry_thread = threading.Thread(target=telemetry.main)
+
+    magnetometer_thread.start()
+    time.sleep(0.5)
+    imu_thread.start()
+    time.sleep(0.5)
+    telemetry_thread.start()
+
+    magnetometer_thread.join()
+    imu_thread.join()
+    telemetry_thread.join() 
+
