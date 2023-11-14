4bcda87b74c79714b0531a852d3f9ad7825e8b48
deochand <deochand@umich.edu>
2023-11-14 01:18:49 -0500

    finished decoder

--- comms/packetDecoder.py
+++ comms/packetDecoder.py
@@ -1,6 +1,19 @@
 # take in txt file
 import binascii
 import pdb
+import csv
+import ast
+import os
+import time
+import datetime
+from pathlib import Path
+
+#########################
+
+# TO DO: FIX FILENUM IN TX, CHANGE THUMBNAIL OUTPUT FOLDER, 
+
+#########################
+
 
 def extract_bytes(hex_string):
     # Convert the hex string to bytes
@@ -30,7 +43,42 @@ result = process_txt_file(file_path)
 image_byte_array = []
 image_dict = {}
 
-# First, create a list of just image data
+# File class
+class File:
+    def __init__(self, total_parts):
+        self.total_parts = total_parts
+        self.all_file_parts = []
+
+def reconstructImage(file, filenum):
+    with open('reconstructed_image'+str(filenum)+'.jpg','wb') as image_file:
+        byte_array = b''.join(file.all_file_parts)
+        image_file.write(byte_array)
+
+def create_file():
+    
+    csv_file2 = Path("transmitted_telem", "unencoded_telem.csv")
+
+    with open(csv_file2, 'w', newline='') as csvfile2:
+        # Write the header row
+        csvwriter = csv.writer(csvfile2)
+        
+        csvwriter.writerow(
+            ["UnixTime", "PacketCount", "GyroX", "GyroY", "GyroZ", "AccelX", "AccelY", "AccelZ", "MagX", "MagY", "MagZ",
+            "BatteryTemp1", "BatteryTemp2", "BMETemp", "BMEPressure", "BMEHumidity", "UsedRam", "FreeMemory", "CPULoad",
+            "CPUTemp", "BattRawVoltage", "BattRawCurrent", "V3v3", "I3v3", "V5v0", "I5v0", "VvBatt", "IvBatt", "RegTemp3v3","RegTemp5v0",
+            "GPSLat", "GPSLatNS", "GPSLn", "GPSLonEW","GPSAlt", "GPSVelocity","GPSSNR","Mag1X","Mag1Y","Mag1Z","Mag2X", "Mag2Y", "Mag2Z" ])
+    return csv_file2
+    
+def write_line(data_dict, csv_file2):
+    with open(csv_file2, mode='a') as file:
+        writer = csv.writer(file)
+        new_data = list(data_dict.values())
+        writer.writerow(["None" if data is None else data for data in new_data])
+
+# First, create a list of just image data, assuming first file number is 0
+current_filenum = 0
+create_file()
+
 for byte_array in result:
     PID = byte_array[0] # type int
     length = int.from_bytes(byte_array[1:2],byteorder='little')
@@ -38,34 +86,85 @@ for byte_array in result:
     file_part = int.from_bytes(byte_array[5:6],byteorder='little')
     total_parts = int.from_bytes(byte_array[7:8],byteorder='little')    
     data = byte_array[9:]
-
-    #pdb.set_trace()
+    
     
     # See if we have an image
     if PID == 69:
-        image_dict[str(file_num)] = {
-            'Length': length,
-            'File Part': file_part,
-            'Total Parts': total_parts,
-            'Data': data
-        }
-
-for outer_key, inner_dict in image_dict.items():
-    if 
-
-
-       
-
-#for byte_array in image_byte_array:
-    #print(byte_array[0])
-    #print(int.from_bytes(byte_array[0],byteorder='little'))
         
+        # Check to see if we need to make a new file and add to dictionary or append this data to existing file
+        if file_num != current_filenum:
+            image_dict[str(file_num)] = {
+            'file': File(total_parts=total_parts)
+            }
 
-# Print the result (list of byte arrays)
-# for i, byte_array in enumerate(result):
-#     print(f"Packet {i + 1}: {byte_array}")
+            image_dict[str(file_num)]['file'].all_file_parts.append(data)
+
+            current_filenum = file_num
+            #print(len(image_dict[str(file_num)]['file'].all_file_parts))
+        else:
+            image_dict[str(file_num)]['file'].all_file_parts.append(data)
+            #print(len(image_dict[str(file_num)]['file'].all_file_parts))
+    # Otherwise, we have telemetry data
+    else:
+        data_dict = {}        
+
+        with open('/Users/amal/Desktop/CubeSat/Code/team-papa/comms/CFL_beacon_def.csv', 'r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+
+            # Function to actually do the conversion function
+            def apply_conversion(expression, X):
+                try:
+                    result = eval(expression, {'X': X})
+                    return result
+                except Exception as e:
+                    return f"Error: {e}"
+
+            for row in csv_reader:
+                title = row['Title']
+                data_type = row['Data Type']
+                offset = row['Offset']
+                size = row['Size']
+                is_signed = True if int(row['Signed'])==1 else False
+                decode_conversion_function = row['Decode Conversion Function']
+                encode_conversion_function = row['Encode Conversion Function']
+                unit = row['Unit']
+                comments = row['Comments']
+                encoded_X = byte_array[int(offset):int(offset)+int(size)]
+
+                #print(encoded_X)
+                #pdb.set_trace()
+
+                data_dict[title] = {
+                    "Type": data_type,
+                    "Offset": offset,
+                    "Size": int(size),
+                    "Signed": is_signed,
+                    "Decoding Conversion": decode_conversion_function,
+                    "Encoding Conversion": encode_conversion_function,
+                    "Unit": unit,
+                    "Comments": comments,
+                    "Unencoded X": apply_conversion(decode_conversion_function,int.from_bytes(encoded_X,byteorder='little',signed=is_signed)),
+                    "Encoded X": encoded_X
+                }
+                #pdb.set_trace()
+        unencoded_x_values = {key: entry["Unencoded X"] for key, entry in data_dict.items() if "Unencoded X" in entry}    
+
+        # WRITE TO CSV 
+        csv_file2 = Path("transmitted_telem", "unencoded_telem.csv")
+        write_line(unencoded_x_values, csv_file2)
+                
+        #pdb.set_trace()
+
+# Finally, go through all files in dictionary, and if we have any complete files, run reconstruct method and save down thumbnails
+for outer_key, inner_dict in image_dict.items():
+    if len(inner_dict['file'].all_file_parts) == inner_dict['file'].total_parts:
+        reconstructImage(inner_dict['file'], outer_key)
+
+            
+#pdb.set_trace()
 
 
 
-# with open('reconstructed_image.jpg','wb') as image_file:
-#     image_file.write(byte_array)
+
+
+
